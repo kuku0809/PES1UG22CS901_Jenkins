d3a461f3854c8d6cc0451ee8bb8a34ce3b7214de
Kusuma Sanjaykumar <128202181+kuku0809@users.noreply.github.com>
2025-03-13 19:03:57 +0530

    Create hell.cpp

---
+++ main/hell.cpp
@@ -0,0 +1,10 @@
+//File: hell.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "New cpp file\n"; 
+  cout << "Hello, Jenkins-Pipeline\n"; 
+  cout<<"PES1UG22CS901-for task5n";
+  return 0;
+}
